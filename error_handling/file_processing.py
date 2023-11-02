a67c217288749d8d48b6c51436cc506f9e281178
Ilya <magnet7777@yandex.ru>
2023-11-03 00:07:01 +0300

    Completed practical work num 5

--- error_handling/file_processing.py
+++ error_handling/file_processing.py
@@ -1,11 +1,20 @@
 def processing(file: str):
-    f = open(file, mode='r', encoding='utf8')
-    count_num = int(f.readline())
+    try:
+        f = open(file, mode='r', encoding='utf8')
 
-    numbers = []
-    for i in range(0, count_num):
-        numbers.append(int(f.readline()))
+        count_num = int(f.readline())
 
-    f.close()
+        numbers = []
+        for i in range(0, count_num):
+            numbers.append(int(f.readline()))
 
-    return numbers
+        f.close()
+
+    except FileNotFoundError:
+        return "Файл не найден"
+    except OSError:
+        return "Ошибка операционной системы"
+    except:
+        return "Произошла непредвиденная ошибка!"
+    else:
+        return numbers
--- error_handling/viewier.py
+++ error_handling/viewier.py
@@ -2,9 +2,14 @@ from error_handling.file_processing import *
 
 
 def show():
-    file = input("Введите имя файла: ")
+    print("Введите \"выход\" чтобы завершить программу.")
+    while True:
+        file = input("Введите имя файла: ")
 
-    print(processing(file))
+        if file == "выход":
+            break
+
+        print(processing(file))
 
 
 if __name__ == "__main__":
