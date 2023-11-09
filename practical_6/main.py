88fb3e271d630ab5bca40e2183319b9e39b0cfba
Ilya <magnet7777@yandex.ru>
2023-11-09 16:40:23 +0300

    Completed practical 6

--- practical_6/main.py
+++ practical_6/main.py
@@ -1,22 +1,28 @@
 from datetime import datetime
 
-file = open("schedule.txt", mode="r", encoding="utf8")
 
-lst = [x.replace('\n', '') for x in file.readlines()]
-lst_raise = []
+def main():
+    file = open("schedule.txt", mode="r", encoding="utf8")
 
-for i in lst:
-    new_lst = i.split()
-    if 'Рейс' == new_lst[0] and new_lst[1].isdigit() and \
-        (("прибыл" == new_lst[2] and "из" == new_lst[3]) or \
-        ("отправился" == new_lst[2]  and "в" == new_lst[3])) and \
-        new_lst[4].istitle() and new_lst[5] == 'в' and datetime.strptime(new_lst[6], "%H:%M:%S"):
-        lst_raise.append(f'[{new_lst[6]}] - Поезд № {new_lst[1]} {new_lst[3]} {new_lst[4]}')
-print(lst_raise)
+    lst = [x.replace('\n', '') for x in file.readlines()]
+    lst_raise = []
 
-file.close()
+    for i in lst:
+        new_lst = i.split()
+        if 'Рейс' == new_lst[0] and new_lst[1].isdigit() and \
+            (("прибыл" == new_lst[2] and "из" == new_lst[3]) or \
+            ("отправился" == new_lst[2]  and "в" == new_lst[3])) and \
+            new_lst[4].istitle() and new_lst[5] == 'в' and datetime.strptime(new_lst[6], "%H:%M:%S"):
+            lst_raise.append(f'[{new_lst[6]}] - Поезд № {new_lst[1]} {new_lst[3]} {new_lst[4]}')
+    print(lst_raise)
 
-file = open("raise.txt", mode="w", encoding="utf8")
+    file.close()
 
-for i in lst_raise:
-    file.write(f'{i}\n')
+    file = open("raise.txt", mode="w", encoding="utf8")
+
+    for i in lst_raise:
+        file.write(f'{i}\n')
+
+
+if __name__ == "__main__":
+    main()
---
+++ practical_6/viewier.py
@@ -0,0 +1,9 @@
+from practical_6.main import *
+
+
+def show():
+    main()
+
+
+if __name__ == "__main__":
+    show()
