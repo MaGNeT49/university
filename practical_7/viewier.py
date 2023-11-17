a16810a12de56e8ffbf22afea26170f4ac01ebb5
magnet <magnet7777@yandex.ru>
2023-11-17 10:39:27 +0300

    completed practical 7

--- practical_7/viewier.py
+++ practical_7/viewier.py
@@ -2,13 +2,16 @@ from practical_7.main import *
 
 
 def show():
-    f = open("books.csv", "r", encoding="utf-8")
-    reader_ = csv.reader(f, delimiter='|')
-    book_ = get_books("python")
-    print(book_)
-    print(get_totals(book_))
-    print(get_totals(list(reader_)))
-    f.close()
+
+    print("Чтобы выйти введите \"0\".")
+
+    while True:
+        input_user = input("Введите название книге: ")
+        if input_user == '0':
+            break
+        data = get_books(input_user)
+        print(data)
+        print(get_totals(data))
 
 
 if __name__ == "__main__":
