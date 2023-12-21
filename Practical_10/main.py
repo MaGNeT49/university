6b2e28bb22563a0bb39d13fa71335f39b2ab3978
magnet <magnet7777@yandex.ru>
2023-12-21 16:22:14 +0300

    edit practical 10

--- Practical_10/main.py
+++ Practical_10/main.py
@@ -15,14 +15,24 @@ def pdf_to_docx_window():
 
 
 def main_window():
-    layout = [
+    is_pdf_docx = True if len(get_files_pdf(get_current_catalog())) == 0 else False
+
+    left_col = [
         [sg.Text(f'Текущая дериктория: {get_current_catalog()}', key="TEXT")],
         [sg.Button('Сменить рабочий каталог', key="0")],
-        [sg.Button('Преобразовать PDF в Docx', key="1")],
+        [sg.Button('Преобразовать PDF в Docx', key="1", disabled=is_pdf_docx)],
         [sg.Button('Преобразовать Docx в PDF', key="2")],
         [sg.Button('Произвести сжатие изображения', key="3")],
         [sg.Button('Удалить группу файлов', key="4")],
         [sg.Button('Выход')]
+    ]
+
+    right_col = [
+        [sg.Text("")],
+        [sg.Listbox([], size=(20, 10), key='-LISTBOX-', select_mode=sg.LISTBOX_SELECT_MODE_EXTENDED, enable_events=True)],
+    ]
+    layout = [
+        sg.vtop([sg.Column(left_col, element_justification='l'), sg.Col(right_col, element_justification='c')])
     ]
 
     return sg.Window('Office Tweaks', layout, finalize=True)
@@ -36,9 +46,11 @@ while True:
         break
 
     if event == "0":
+        file_list = get_files_format(get_current_catalog(), "pdf")
         direct = sg.popup_get_folder('Выберите папку', title='Диалог выбора папки', default_path=get_current_catalog())
         changing_directory(direct)
         window["TEXT"].update(f'Текущая дериктория: {get_current_catalog()}')
+        window['-LISTBOX-'].Update(os.listdir(get_current_catalog()))
 
     if window == window1 and event == '1':
         window2 = pdf_to_docx_window()
